73cad3b0e3ae03e5f3b05246d77bbe1a4dd9c23e
Lucas-Guelfi <lucas.guelfi@nortox.com.br>
2024-10-17 14:26:47 -0300

    teste

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -29,7 +29,7 @@ export class ListarTarefaComponent implements OnInit {
   }
   
   alterarStatus(tarefa: Tarefa): void {
-    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+    if (confirm('Deseja alterar o status da  tarefa "' + tarefa.nome + '"?')) {
       this.tarefaService.alterarStatus(tarefa.id);
       this.tarefas = this.tarefaService.listarTodos();
     }
